61cce93a80e3ff0f94cf0fe73beadff5b9099d95
divotlt <150188672+divotlt@users.noreply.github.com>
2024-02-05 19:01:17 -0800

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,1 +1,16 @@
+// counter.js
+export function createCounter(initialValue = 0) {
+  let value = initialValue;
 
+  return {
+    increment() {
+      value++;
+    },
+    decrement() {
+      value--;
+    },
+    getValue() {
+      return value;
+    },
+  };
+}
