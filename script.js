160361d4bd7f08d8f61bfd69d2ac4d5444e650ca
divotlt <150188672+divotlt@users.noreply.github.com>
2024-02-06 14:35:06 -0800

    move

--- script.js
+++
@@ -1,8 +0,0 @@
-// main.js
-import { createCounter } from './counter.js';
-
-const myCounter = createCounter(10);
-
-console.log('Initial value:', myCounter.getValue()); // 10
-myCounter.increment();
-console.log('After increment:', myCounter.getValue()); // 11
