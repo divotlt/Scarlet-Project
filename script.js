264220966d29a01293cae417d84de4785f75d503
divotlt <150188672+divotlt@users.noreply.github.com>
2024-02-05 19:01:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// main.js
+import { createCounter } from './counter.js';
 
+const myCounter = createCounter(10);
+
+console.log('Initial value:', myCounter.getValue()); // 10
+myCounter.increment();
+console.log('After increment:', myCounter.getValue()); // 11
